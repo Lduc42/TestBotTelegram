086c9e6ce2655c76d608205e2e1236c70c855709
VAN DUC LE <100124747+Lduc42@users.noreply.github.com>
2025-01-16 10:36:43 +0700

    update scripte

--- Scripts/Bot.py
+++ Scripts/Bot.py
@@ -1,24 +1,24 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes
 import requests
 
 # ID của ứng dụng trên Google Play
-APP_ID = "com.ducminh.helix.jumping.journey"
+APP_ID = "com.ducminh.tripple.match.finding"
 
 async def check_app_status():
     """Kiểm tra trạng thái của ứng dụng trên Google Play."""
     url = f"https://play.google.com/store/apps/details?id={APP_ID}"
     response = requests.get(url)
     if response.status_code == 200:
-        return f"✅ Ứng dụng `{APP_ID}` vẫn còn tồn tại trên Google Play."
+        return f"✅ Ứng dụng vẫn còn tồn tại trên Google Play."
     else:
-        return f"❌ Ứng dụng `{APP_ID}` đã bị gỡ hoặc không tồn tại."
+        return f"❌ Ứng dụng đã bị gỡ hoặc không tồn tại."
 
-async def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Gửi tin nhắn chào mừng."""
     await update.message.reply_text("Chào bạn! Tôi sẽ tự động kiểm tra trạng thái ứng dụng trên Google Play.")
 
-async def check(update: Update, context: CallbackContext):
+async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Kiểm tra trạng thái ứng dụng bằng ID mặc định."""
     status = await check_app_status()  # Gọi hàm kiểm tra với APP_ID mặc định
     await update.message.reply_text(status)
